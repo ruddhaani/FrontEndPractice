828c63b3bc16e2fa18cea0fdd63c309c3ffc1013
ruddhaani <ramaneaniruddh22@gmail.com>
2025-01-06 10:43:49 +0530

    search and pagination added

--- ToDoList-Javascript/index.js
+++ ToDoList-Javascript/index.js
@@ -1,25 +1,46 @@
 // let todoBoxElement = document.createElement("div");
 // todoBoxElement.classList
 
+let containerElement = document.querySelector(".container");
+
 let toDoList = [
     {
-        "task" : "Complete C Homework.",
-        "urgency" : "Immediate" 
+        "task": "Complete C Homework.",
+        "urgency": "Immediate"
     },
     {
-        "task" : "Complete C# Homework.",
-        "urgency" : "Immediate" 
+        "task": "Complete C# Homework.",
+        "urgency": "Immediate"
     },
 ]
 
-let showToDoList = (index , searchText="") => {
-    let containerElement = document.querySelector(".container");
+let showToDoList = (index, searchText = "", pageNumber = 1, pageSize = 2) => {
     containerElement.innerHTML = "";
 
+    let filteredElementCount = toDoList.filter((x) => x.task.toLowerCase().search(searchText.toLowerCase) == -1).length;
+    console.log(filteredElementCount);
+
+    let fromIndex = (pageNumber-1) * pageSize;
+    let toIndex = Math.min(fromIndex + pageSize - 1 , filteredElementCount - 1);
+
+    let totalPages = Math.floor(filteredElementCount / pageSize);
+    let count = -1;
+
+    if(filteredElementCount % pageSize != 0){
+        totalPages ++;
+    }
+
     for (let key in toDoList) {
-        if(toDoList[key].task.toLowerCase().search(searchText.toLowerCase()) == -1){
+        if (toDoList[key].task.toLowerCase().search(searchText.toLowerCase()) == -1) {
             continue;
         }
+
+        count++;
+
+        if(!(count >=fromIndex && count <=toIndex)){
+            continue;
+        }
+
         let todoBoxElement = document.createElement("div");
         todoBoxElement.classList.add("toDoBox");
         let innerHTMLString = `
@@ -28,7 +49,7 @@ let showToDoList = (index , searchText="") => {
                 <h4>Urgency : ${toDoList[key].urgency}</h4>
             </div>
             <div class="toDoButtons">
-                <button onclick="updateTask(${key}, 0)">Update</button>
+                <button onclick="updateTask(${key}, 0 , ${pageNumber} , ${searchText})">Update</button>
                 <button onclick="deleteTask(${key})">Delete</button>
             </div>
         `;
@@ -40,7 +61,7 @@ let showToDoList = (index , searchText="") => {
                     <h4>Urgency : <input type="text" id="updatedUrgency" value="${toDoList[key].urgency}"></h4>
                 </div>
                 <div class="toDoButtons">
-                    <button onclick="updateTask(${key}, 1)">Update</button>
+                    <button onclick="updateTask(${key}, 1 , ${pageNumber} , ${searchText})">Update</button>
                     <button onclick="deleteTask(${key})">Delete</button>
                 </div>
             `;
@@ -49,17 +70,25 @@ let showToDoList = (index , searchText="") => {
         todoBoxElement.innerHTML = innerHTMLString;
         containerElement.appendChild(todoBoxElement);
     }
+
+    for(let i = 1 ; i <=totalPages ; i++){
+        let buttonElement = document.createElement("button");
+        buttonElement.innerHTML = `${i}`;
+        buttonElement.addEventListener('click' , getPage);
+        containerElement.appendChild(buttonElement);
+        
+    }
 };
 
 showToDoList(-1);
 
-let addToList = ()=>{
+let addToList = () => {
     let task = document.getElementById("task").value;
     let urgency = document.getElementById("urgency").value;
 
     let newTask = {
-        "task" : task,
-        "urgency" : urgency
+        "task": task,
+        "urgency": urgency
     }
 
     toDoList.push(newTask);
@@ -70,17 +99,17 @@ let addToList = ()=>{
     showToDoList(-1);
 }
 
-let deleteTask = (index)=>{
-    toDoList.splice(index,1);
+let deleteTask = (index) => {
+    toDoList.splice(index, 1);
 
     showToDoList(-1);
 
 }
 
-let updateTask = (index , flag) =>{
-    if(flag === 0){
-        showToDoList(index);
-    }else{
+let updateTask = (index, flag , pageNumber ,searchText) => {
+    if (flag === 0) {
+        showToDoList(index , searchText , pageNumber = pageNumber );
+    } else {
         let task = document.getElementById("updatedTask").value;
         let urgency = document.getElementById("updatedUrgency").value;
 
@@ -88,12 +117,19 @@ let updateTask = (index , flag) =>{
 
         taskOld.task = task;
         taskOld.urgency = urgency;
-        showToDoList(-1);
+        showToDoList(-1 , searchText , pageNumber = pageNumber);
     }
 }
 
-let searchTask = ()=>{
+let searchTask = () => {
     let searchText = document.getElementById("searchBox").value;
 
-    showToDoList(-1,searchText);
+    showToDoList(-1, searchText);
+}
+
+function getPage(event){
+    console.log(event.target.textContent);
+    let pageNumber = Number(event.target.textContent);
+
+    showToDoList(-1 ,"",pageNumber ,2);
 }
