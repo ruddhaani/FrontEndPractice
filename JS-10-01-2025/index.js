6ee183ae8885415d6a0f5780b6ff1d0c0493f33a
ruddhaani <ramaneaniruddh22@gmail.com>
2025-01-10 23:03:28 +0530

    done with update function

--- JS-10-01-2025/index.js
+++ JS-10-01-2025/index.js
@@ -2,42 +2,79 @@ let apiEndPoint = "http://localhost:5036/api/Employees/";
 
 let containerElement = document.querySelector(".container");
 
-async function getEmployees(){
+async function getEmployees() {
     try {
         const response = await fetch(`${apiEndPoint}`);
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error(`Failed to fetch Employee: ${response.status}`);
         }
 
         const data = await response.json();
-        console.log(data)
         return data;
     } catch (error) {
         console.log(error);
     }
 }
 
-// getEmployees();
-
-const addEmployeeToDB = async ( name , email , salary)=> {
+async function getEmployeesById(id) {
     try {
-        const response = await fetch("http://localhost:5036/api/Employees" , {
-            method : 'POST',
+        const response = await fetch(`${apiEndPoint}${id}`);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function updateEmployeeInDB(id, name, email, salary) {
+    try {
+        const response = await fetch(`${apiEndPoint}${id}`, {
+            method: 'PUT',
             headers: {
-                'Content-Type' : 'application/json', 
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                Name : name,
-                Email : email,
-                Salary : salary
+                Name: name,
+                Email: email,
+                Salary: salary
             })
         });
-    
-        if(!response.ok){
-            throw new Error(`Error: ${response.status}`); 
+
+        if (!response.ok) {
+            throw new Error(`Error : ${response.status}`);
         }
-    
+
+        return await response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const addEmployeeToDB = async (name, email, salary) => {
+    try {
+        const response = await fetch("http://localhost:5036/api/Employees", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Name: name,
+                Email: email,
+                Salary: salary
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
         const data = await response.json()
         console.log(data);
     } catch (error) {
@@ -47,14 +84,14 @@ const addEmployeeToDB = async ( name , email , salary)=> {
 
 //Delete Employee
 
-async function deleteEmployeeFromDB(id){
+async function deleteEmployeeFromDB(id) {
     try {
         let employeeId = Number(id);
-        const response = await fetch(`${apiEndPoint}${employeeId}`,{
+        const response = await fetch(`${apiEndPoint}${employeeId}`, {
             method: 'DELETE',
         });
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error(`error: ${response.status}`);
         }
 
@@ -64,33 +101,45 @@ async function deleteEmployeeFromDB(id){
     }
 }
 
-async function addEmployee(){
+async function addEmployee() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let salary = Number(document.getElementById("salary").value);
 
-    if(name === "" || email === "" || salary <= 0){
+    if (name === "" || email === "" || salary <= 0) {
         alert("Please add valid data");
         return;
     }
 
-    await addEmployeeToDB(name , email , salary);
+    await addEmployeeToDB(name, email, salary);
     showEmployees();
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("salary").value = "";
 }
 
 async function deleteEmployee(id) {
     let employeeId = Number(id);
 
     await deleteEmployeeFromDB(employeeId);
-    await showEmployees();
+    showEmployees();
 }
 
-async function showEmployees(){
+async function showEmployees(searchText = "", id = -1, index = -1) {
     let employees = await getEmployees();
 
     containerElement.innerHTML = "";
-    for(var emp in employees){
+    for (var emp in employees) {
         let employee = employees[emp];
+
+        if (employee.name.toLowerCase().search(searchText.toLowerCase()) == -1) {
+            continue;
+        }
+
+        if (id > 0 && employee.employeeId != id) {
+            continue;
+        }
 
         let employeeList = document.createElement("div");
         employeeList.classList.add("EmployeeList")
@@ -101,10 +150,55 @@ async function showEmployees(){
                 <li>Salary: ${employee.salary}</li>
             </ul>
             <button onclick="deleteEmployee(${employee.employeeId})">Delete Employee</button>
+            <br>
+            <button onclick = "updateEmployee(${emp}, 0 ,${employee.employeeId})">Update Employee</button>
             `
 
+        if (index >= 0 && index < employees.length && index == emp) {
+            employeeList.innerHTML = `<ul>
+                <li>Employee Id: ${employee.employeeId}</li>                
+                <li>Name: <input type="text" name="UpdatedName" id="UpdatedName" value="${employee.name}"></li>
+                <li>Email: <input type="text" name="UpdatedEmail" id="UpdatedEmail" value="${employee.email}"></li>
+                <li>Salary: <input type="text" name="UpdatedSalary" id="UpdatedSalary" value="${employee.salary}"></li>
+            </ul>
+            <button onclick="deleteEmployee(${employee.employeeId})">Delete Employee</button>
+            <br>
+            <button onclick = "updateEmployee(${emp}, 1 ,${employee.employeeId})">Update Employee</button>
+            `
+        }
+
         containerElement.appendChild(employeeList);
+
+    }
+}
+
+async function searchWithName() {
+    let searchText = document.getElementById("search").value;
+
+    showEmployees(searchText);
+
+}
+
+async function showEmployeeById() {
+    let id = Number(document.getElementById('searchById').value);
+    console.log(id);
+
+    showEmployees("", id);
+
+    document.getElementById('searchById').value = "";
+}
+
+async function updateEmployee(index, flag , id) {
+    if(flag == 0){
+        await showEmployees("" , -1 , index);
+        console.log(index); 
+    }else{
+        let _name = document.getElementById("UpdatedName").value;
+        let _email = document.getElementById("UpdatedEmail").value;  
+        let _salary = Number(document.getElementById("UpdatedSalary").value);
         
+        await updateEmployeeInDB(id , _name , _email , _salary);
+        showEmployees();
     }
 }
 
