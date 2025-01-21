69ebfe313907a26156726b213147c1231150c8d2
ruddhaani <ramaneaniruddh22@gmail.com>
2025-01-21 10:03:19 +0530

    commit

--- React Websites/my-vue-app/src/main.jsx
+++ React Websites/my-vue-app/src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const tableElement = (
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Id</th>
+      <th>Email</th>
+    </tr>
+    <tr>
+      <td>Ani</td>
+      <td>123</td>
+      <td>ramaneaniruddha22@gmail.com</td>
+    </tr>
+  </table>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  tableElement
 )
