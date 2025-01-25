938d8bbb4f7237041f8fcbe9c0355b0f810692c3
ruddhaani <ramaneaniruddh22@gmail.com>
2025-01-25 22:36:18 +0530

    update functionality added

--- React Websites/CRUDPractice/src/components/StudentTable.jsx
+++ React Websites/CRUDPractice/src/components/StudentTable.jsx
@@ -6,6 +6,13 @@ const StudentTable = () => {
   const [pageSize, setPageSize] = useState(2);
   const [totalPages, setTotalPages] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
+  const [updateStudentFlag, setUpdateStudentFlag] = useState(true);
+  const [updateStudentId, setUpdateStudentId] = useState(-1);
+  const [updateStudentData, setUpdateStudentData] = useState({
+    name: "",
+    email: "",
+    department: "",
+  });
   const [addStudentData, setAddStudentData] = useState({
     name: "",
     email: "",
@@ -61,6 +68,42 @@ const StudentTable = () => {
   function handleChange(e) {
     setAddStudentData({ ...addStudentData, [e.target.name]: e.target.value });
     console.log(addStudentData);
+  }
+
+  function handleUpdateChange(e) {
+    setUpdateStudentData({
+      ...updateStudentData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(updateStudentData);
+  }
+
+  async function handleUpdate(studentId) {
+    try {
+      const response = await fetch(
+        `http://localhost:5152/api/Student/${studentId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updateStudentData),
+        }
+      );
+
+      setUpdateStudentFlag(false);
+      setUpdateStudentId(-1);
+      setUpdateStudentData({
+        name: "",
+        email: "",
+        department: "",
+      });
+
+      await getStudentData(pageNumber);
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleSubmit(e) {
@@ -129,24 +172,83 @@ const StudentTable = () => {
             <th>Actions</th>
           </tr>
 
-          {studentList.map((student) => (
-            <tr>
-              <td>{student.studentId}</td>
-              <td>{student.name}</td>
-              <td>{student.email}</td>
-              <td>{student.department}</td>
-              <td>
-                <button>Update</button>{" "}
-                <button
+          {studentList.map((student) => {
+            let updateCondition =
+              updateStudentFlag && updateStudentId === student.studentId;
+            return (
+              <tr>
+                <td>{student.studentId}</td>
+                {updateCondition ? (
+                  <td>
+                    <input
+                      type="text"
+                      name="name"
+                      value={updateStudentData.name}
+                      onChange={handleUpdateChange}
+                    ></input>
+                  </td>
+                ) : (
+                  <td>{student.name}</td>
+                )}
+
+                {updateCondition ? (
+                  <td>
+                    <input
+                      type="text"
+                      name="email"
+                      value={updateStudentData.email}
+                      onChange={handleUpdateChange}
+                    ></input>
+                  </td>
+                ) : (
+                  <td>{student.email}</td>
+                )}
+                {updateCondition ? (
+                  <td>
+                    <input
+                      type="text"
+                      name="department"
+                      value={updateStudentData.department}
+                      onChange={handleUpdateChange}
+                    ></input>
+                  </td>
+                ) : (
+                  <td>{student.department}</td>
+                )}
+                {
+                  updateCondition ? <td><button onClick={() => {handleUpdate(student.studentId)}}> Update </button> <button
                   onClick={() => {
                     deleteStudent(student.studentId);
                   }}
                 >
                   Delete
-                </button>
-              </td>
-            </tr>
-          ))}
+                </button></td> :  <td>
+                  <button
+                    onClick={() => {
+                      setUpdateStudentFlag(true);
+                      setUpdateStudentId(student.studentId);
+                      setUpdateStudentData({
+                        name: student.name,
+                        email: student.email,
+                        department: student.department,
+                      });
+                    }}
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteStudent(student.studentId);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+                }
+                
+              </tr>
+            );
+          })}
         </table>
         <div>
           {totalPagesArray.map((i) => {
