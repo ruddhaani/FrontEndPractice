e8abe431bd3a69d32355d6c216350446a373d514
Aniruddha Ramane <ramaneaniruddh22@gmail.com>
2025-02-11 00:32:28 +0530

    counter done

---
+++ React Websites/angular-practice/src/app/components/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+    count : number = 0;
+
+    incrementByOne(){
+      this.count++;
+    }
+}
