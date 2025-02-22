e18627f000f25c860bce0335c9b84db605c29305
Aniruddha Ramane <ramaneaniruddh22@gmail.com>
2025-02-22 09:25:30 +0530

    commit

--- Angular/helloworld.ts
+++ Angular/helloworld.ts
@@ -1,3 +1,33 @@
+function Component(metadata : any){
+    function ComponentDecorator(target : Function){
+        console.log("This class is component class");
+        console.log(metadata.selector);
+    }
+
+    return ComponentDecorator;
+}
+
+function Input(metadata : any){
+    function InputDecorator(target : Function , property:number){
+        console.log("This class is component class");
+        console.log(metadata.selector);
+    }
+
+    return InputDecorator;
+}
+
+
+
+
+
+@Component({
+    "selector" : "app-employee",
+    "templateUrl" : "../employee.html",
+    "styleUrl" : "../employee.css",
+    "imports"  : [
+
+    ]
+})
 class Employee{
     employeeId: number;
     name : string;
@@ -9,8 +39,9 @@ class Employee{
         this.name = name;
         this.email = email;
         this.salary = salary;
+        console.log("Constructor is called");
     }
 }
 
 let employee : Employee = new Employee(1, "Ram" , "ram124@gmail.com" , 10000);
-console.log(employee);
+//console.log(employee);
